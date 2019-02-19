f449568f031e0c14638a2bba5e4809de69609f5a
RonaldHR <RonaldHR@github.com>
2019-02-19 14:28:11 +0100

    Changed pneumatic robot map

--- HRDeepSpace2019/src/main/java/frc/robot/RobotMap.java
+++ HRDeepSpace2019/src/main/java/frc/robot/RobotMap.java
@@ -33,8 +33,8 @@ public class RobotMap {
     public static final int TOPGRABSTOP = 4;
 
     //PCM
-    public static int EXTENDERVALVE = 0;
-    public static int GRABVALVE = 1;
+    public static int EXTENDERVALVE = 7;
+    public static int GRABVALVE = 6;
     public static int BACKPISTON = 2;
     public static int FRONTPISTON = 3;
 
